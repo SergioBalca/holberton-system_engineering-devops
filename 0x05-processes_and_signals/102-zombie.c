a1e80b9dedf9030f63e3a30e30e403c0ecd09bbf
SergioBalca <3896@holbertonschool.com>
2022-02-21 13:27:31 -0800

    program that creates 5 zombie processes

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+
+int main(void)
+{
+	i = 0;
+
+	while (i < 5)
+	{
+		pid_t child_pid = fork();
+	}
+
+	infinite_while;
+}
+
+
+/**
+ * infinite_while- function that stops the execution
+ * of a group of zombie processes
+ * Return: Always 0
+ */
+
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
