bf54eddf75b72e0976b5947ff5e5f6a3023338d4
SergioBalca <3896@holbertonschool.com>
2022-05-05 11:11:03 -0700

    set the limit higher to solve great amount of requests problem

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # Set the limit higher to solve great amount of request problem
 
-exec {'ULIMIT':
+exec {'changeLimit':
     command => 'sed -i "/s/15/4096/g" /etc/default/nginx; service restart',
     path    => '/usr/bin/:/bin',
 }
