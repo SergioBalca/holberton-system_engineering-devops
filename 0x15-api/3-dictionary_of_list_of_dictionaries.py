5ae1d34815934cc4f3c44e7c498599b63c3df2ac
SergioBalca <git@github.com:SergioBalca>
2022-05-19 22:46:30 -0700

    Python script to export data in the JSON format

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Python script to export data in the JSON format"""
+
+from csv import QUOTE_ALL, writer
+import json
+from requests import get
+from sys import argv
+
+if __name__ == '__main__':
+
+    users = get('https://jsonplaceholder.typicode.com/users').json()
+    todos = get('https://jsonplaceholder.typicode.com/todos').json()
+
+    dict = {}
+    dict_list = []
+    json_file = 'todo_all_employees.json'
+    with open(json_file, 'w', encoding='utf8') as f:
+        for item in users:
+            USER_ID = item.get('id')
+            USERNAME = item.get('username')
+            for element in todos:
+                TASK_TITLE = element.get('title')
+                TASK_COMPLETED_STATUS = element.get('completed')
+                user_tasks = {
+                    'task':  TASK_TITLE,
+                    'completed': TASK_COMPLETED_STATUS,
+                    'username': USERNAME,
+                }
+                dict_list.append(user_tasks)
+                dict[USER_ID] = dict_list
+                json.dump(dict, f)
