073f8d67478dbcf00423e45bad2dbb3fe893a021
SergioBalca <git@github.com:SergioBalca>
2022-05-21 12:41:03 -0700

    requirments

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-"""Module with top_ten recurse function"""
+""" queries the Reddit API and returns a list containing the titles
+        of all hot articles for a given subreddit.
+        If no results are found for the given subreddit,
+        the function should return None
+"""
+
 
 from requests import get
 
