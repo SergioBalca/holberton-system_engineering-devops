f9fde5c28ac4fb32a0c88b95d928ae0edfa107bd
SergioBalca <git@github.com:SergioBalca>
2022-05-21 12:52:15 -0700

    requirements

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,7 +9,7 @@
 from requests import get
 
 
-def recurse(subreddit, hot_list=[], after=''):
+def recurse(subreddit, hot_list=[], after=None):
     """ queries the Reddit API and returns a list containing the titles
         of all hot articles for a given subreddit.
         If no results are found for the given subreddit,
