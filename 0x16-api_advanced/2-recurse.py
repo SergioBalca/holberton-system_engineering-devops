78dc1a4d7e2ba0b21a324ece13fd3283848ae4cf
SergioBalca <git@github.com:SergioBalca>
2022-05-21 13:11:45 -0700

    requirements

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,7 +6,7 @@
 """
 
 
-from requests
+from requests import get
 
 
 def recurse(subreddit, hot_list=[], after=None):
@@ -15,11 +15,10 @@ def recurse(subreddit, hot_list=[], after=None):
         If no results are found for the given subreddit,
         the function should return None
     """
-
-    url = 'https://www.reddit.com/r/{}/hot.json?after={}'
-    .format(subreddit, after)
-    data_json = requests.get(url, headers={'user-agent': 'SergioBalca'},
-                             allow_redirects=False).json()
+    url = 'https://www.reddit.com/r/'
+    url_2 = '{}{}/hot.json?after={}'.format(url, subreddit, after)
+    data_json = get(url_2, headers={'user-agent': 'SergioBalca'},
+                    allow_redirects=False).json()
 
     if 'error' in data_json or data_json['data']['children'] == []:
         return None
